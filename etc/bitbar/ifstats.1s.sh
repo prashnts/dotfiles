81b651f7eaabf7ece0ef6d670f69caee087f56c5
Prashant Sinha <prashant+git@noop.pw>
2016-11-30 21:46:50 +0530

    Bitbar ifstats

--- etc/bitbar/ifstats.1s.sh
+++ etc/bitbar/ifstats.1s.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 export PATH=$PATH:/usr/local/bin
-printf "↓%4s | font=FiraCode-Retina color=#2A9D8F" $(ifstat -bi en0 0.1 1 | tail -n 1 | \
+# printf "↓%4s | font=FiraCode-Retina color=#2A9D8F" $(ifstat -bi en0 0.1 1 | tail -n 1 | \
+#     awk '
+#       {for (i=1;i<=NF;i++) a[i] = sprintf("%d%s",
+#         ($i > 1000) ? $i / 1024 : $i,
+#         ($i > 1000) ? "m" : "k")}
+#       END {print a[1]}')
+
+printf "↓%4s | font=FiraCode-Retina color=#%s" $(ifstat -bi en0 0.1 1 | tail -n 1 | \
     awk '
-      {for (i=1;i<=NF;i++) a[i] = sprintf("%d%s",
-        ($i > 1024) ? $i / 1024 : $i,
-        ($i > 1024) ? "m" : "k")}
-      END {print a[1]}')
+      {printf("%d%s %s",
+        ($1 > 1000) ? $1 / 1024 : $1,
+        ($1 > 1000) ? "m" : "k",
+        ($1 > 1000) ? "A13D63" : \
+          ($1 > 4000) ? "2A9D8F" : \
+            ($1 > 2000) ? "A5C882" : "324376")
+      }')
