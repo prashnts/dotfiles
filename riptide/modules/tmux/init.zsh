28aa13b4c63bacc71be7803b61bf01ece219ee9a
Prashant Sinha <prashant+git@noop.pw>
2016-12-04 23:19:26 +0530

    tmux config

--- riptide/modules/tmux/init.zsh
+++ riptide/modules/tmux/init.zsh
@@ -1,14 +1,12 @@
 
-function ifstats {
-  local tick=$(netstat -i -I en0 -b | grep -m 1 en0 | awk '{print $7,$10}')
-  sleep 0.5
-  local tock=$(netstat -i -I en0 -b | grep -m 1 en0 | awk '{print $7,$10}')
-
-  echo $(echo $tick'\n'$tock | \
-      awk '{i=$1-i; u=$2-u} END {print i / 128 / .5, u / 128 / .5}' | \
-      awk '
-        {for (i=1;i<=NF;i++) a[i] = sprintf("%2d%s",
-          ($i > 1024) ? $i / 1024 : $i,
-          ($i > 1024) ? "m" : "k")}
-        END {printf "⎆ ↑ %s ↓ %s", a[1], a[2]}')
+function get_if_stats() {
+  printf "%s", $(ifstat -bi en0 0.1 1 | tail -n 1 | \
+    awk '
+      {printf("↑ %d%s",
+        ($1 > 1000) ? $1 / 1024 : $1,
+        ($1 > 1000) ? "m" : "k",
+        ($1 > 1000) ? "A13D63" : \
+          ($1 > 4000) ? "2A9D8F" : \
+            ($1 > 2000) ? "A5C882" : "324376")
+      }');
 }
